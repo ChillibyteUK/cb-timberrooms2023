96a8d77c8c9819395e776a1af5d434e6b35ac062
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-21 10:14:52 +0100

    pricing style

--- page-templates/blocks/cb_pricing.php
+++ page-templates/blocks/cb_pricing.php
@@ -8,7 +8,7 @@
         <div class="pricing__card">
             <img class="pricing__image" src="<?=wp_get_attachment_image_url(get_sub_field('image'),'large')?>" alt="">
             <div class="pricing__size"><?=get_sub_field('size')?></div>
-            <div class="pricing__price">£<?=number_format(get_sub_field('price'),2)?></div>
+            <div class="pricing__price">£<?=number_format(get_sub_field('price'),2)?> inc VAT</div>
             <div class="pricing__features">
                 <ul>
                     <?=cb_list(get_sub_field('features'))?>
