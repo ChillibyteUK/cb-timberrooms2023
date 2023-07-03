f24e0f8050a6df417a0123ec05db226688346332
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-03 11:46:18 +0100

    Update cb_pricing.php

--- page-templates/blocks/cb_pricing.php
+++ page-templates/blocks/cb_pricing.php
@@ -6,7 +6,7 @@
         while(have_rows('cards')) {
             the_row();
             ?>
-        <div class="pricing__card" data-aos="fade-up" data-aos-delay="<?=$d?>>
+        <div class="pricing__card" data-aos="fade-up" data-aos-delay="<?=$d?>">
             <img class="pricing__image" src="<?=wp_get_attachment_image_url(get_sub_field('image'),'large')?>" alt="">
             <div class="pricing__size"><?=get_sub_field('size')?></div>
             <div class="pricing__price">£<?=number_format(get_sub_field('price'),2)?> inc VAT</div>
