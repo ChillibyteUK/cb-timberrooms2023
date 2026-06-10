c066fd5110ee6eb9b5c206df35eb60fe0a1bf4c0
ChillibyteUK <dan@chillibyte.co.uk>
2026-06-10 17:22:19 +0100

    phpcs cs archive

--- archive-case-studies.php
+++ archive-case-studies.php
@@ -1,6 +1,11 @@
 <?php
-// Exit if accessed directly.
-defined('ABSPATH') || exit;
+/**
+ * The template for displaying the case studies archive page.
+ *
+ * @package cb-timberrooms2023
+ */
+
+defined( 'ABSPATH' ) || exit;
 
 get_header();
 $hero_img = wp_get_attachment_image_url( get_field( 'case_studies_archive_hero', 'options' ), 'full' );
@@ -62,37 +67,37 @@ $hero_img = wp_get_attachment_image_url( get_field( 'case_studies_archive_hero',
     <div class="container-xl py-5">
         <div class="w-100 mb-4" id="csgrid">
             <?php
-        $d = 0;
-    while (have_posts()) {
-        the_post();
-        $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
-        if (!$img) {
-            $img = get_stylesheet_directory_uri() . '/img/default.png';
-        }
-        $slug = acf_slugify(basename(get_the_permalink()));
-        $catclass = '';
-        ?>
-            <div class="<?=$catclass?> caseStudy" data-aos="fade" data-aos-delay="<?=$d?>">
-                <a class="caseStudy_card" href="<?=get_the_permalink()?>">
+			$d = 0;
+			while ( have_posts() ) {
+				the_post();
+				$img = get_the_post_thumbnail_url( get_the_ID(), 'large' );
+				if ( ! $img ) {
+					$img = get_stylesheet_directory_uri() . '/img/default.png';
+				}
+				$slug     = acf_slugify( basename( get_the_permalink() ) );
+				$catclass = '';
+				?>
+            <div class="<?= esc_attr( $catclass ); ?> caseStudy" data-aos="fade" data-aos-delay="<?= esc_attr( $d ); ?>">
+                <a class="caseStudy_card" href="<?= esc_url( get_the_permalink() ); ?>">
                     <div class="caseStudy_card__image">
-                        <img src="<?=$img?>" alt="">
+                        <img src="<?= esc_url( $img ); ?>" alt="">
                     </div>
                     <div class="caseStudy_card__content">
                         <div class="article-title mb-2">
-                            <?=get_the_title()?>
+                            <?= esc_html( get_the_title() ); ?>
                         </div>
                     </div>
                 </a>
             </div>
-        <?php
-        $d += 50;
-    }
-?>
+				<?php
+				$d += 50;
+			}
+			?>
         </div>
-        <?=numeric_posts_nav()?>
+        <?= numeric_posts_nav(); ?>
     </div>
     <?php
-    include(get_stylesheet_directory() . '/page-templates/blocks/cb_cta.php');
+    require get_stylesheet_directory() . '/page-templates/blocks/cb_cta.php';
     ?>
 </main>
 <?php
